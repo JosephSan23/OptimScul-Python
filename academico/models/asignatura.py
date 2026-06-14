858a01500c7cfe43a885aa5024cbf1b069b290cd
Joseph <josephsantiago2345@gmail.com>
2026-06-13 19:39:35 -0500

    feat: Creando el modelo de asignatura

--- academico/models/asignatura.py
+++ academico/models/asignatura.py
@@ -1,1 +1,13 @@
 from django.db import models
+
+class Asignatura(models.Model):
+    id_asignatura = models.AutoField(primary_key=True)
+    colegio = models.ForeignKey('Colegio', models.DO_NOTHING)
+    nombre_asignatura = models.CharField(max_length=50)
+    
+    class Meta:
+        managed = False
+        db_table = 'asignatura'
+        
+    def __str__(self):
+        return self.nombre_asignatura
