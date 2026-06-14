3415f9d249852005aa859ad5457e44ae701df201
Joseph <josephsantiago2345@gmail.com>
2026-06-14 12:32:51 -0500

    feat: Añadiendo el archivo urls con sus respectivas rutas

---
+++ academico/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+app_name = 'academico'
+
+urlpatterns = [
+    # Pantallas
+    path('mis-cursos/', views.ListaCursosView.as_view(), name='lista_cursos'),
+    path('mis-cursos/<int:curso_id>/materias/', views.ListaMateriasCursoView.as_view(), name='lista_materias_curso'),
+    path('mis-cursos/<int:curso_id>/materias/<int:asignatura_id>/panel/', views.PanelMateriaView.as_view(), name='panel_materia'),
+]
