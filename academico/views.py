9de642862ee8e388dfc38ede62c8c052f7fc4a63
Joseph <josephsantiago2345@gmail.com>
2026-06-15 11:19:45 -0500

    feat: Creando la funcion para mandar datos adicionales al html

--- academico/views.py
+++ academico/views.py
@@ -12,5 +12,11 @@ class ListaCursosView(ListView):
         
         asignaciones = ProfesorCursoAsignatura.objects.filter(id_profesor=profesor_id)
         
+        # distinct elimina duplicados
         curso_ids = asignaciones.values_list('id_curso', flat=True).distinct()
         return Curso.objects.filter(id_curso__in=curso_ids)
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['curso'] = Curso.objects.get(pk=self.kwargs['curso_id'])
+        return context
