297de53e33bfc7e6b5fae86f5253739955a9b5da
Joseph <josephsantiago2345@gmail.com>
2026-06-15 10:33:48 -0500

    feat: Creando una vista basada en clases para mostrar los cursos de un profesor y obtener una lista

--- academico/views.py
+++ academico/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.views.generic import ListView, TemplateView
+from .models import ProfesorCursoAsignatura, Curso, Asignatura
 
 # Create your views here.
+class ListaCursosView(ListView):
+    model = ProfesorCursoAsignatura
+    template_name = 'academico/lista_cursos.html'  
+    context_object_name = 'cursos_asignados'
+
+    def get_queryset(self):
+        profesor_id = 1 
+        
+        asignaciones = ProfesorCursoAsignatura.objects.filter(id_profesor=profesor_id)
+        
+        curso_ids = asignaciones.values_list('id_curso', flat=True).distinct()
+        return Curso.objects.filter(id_curso__in=curso_ids)
