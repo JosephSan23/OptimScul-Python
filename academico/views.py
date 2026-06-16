fd0f5731000b085249888cecbd5a339809eafc4b
Joseph <josephsantiago2345@gmail.com>
2026-06-15 19:41:15 -0500

    feat: crear flujo de navegación cursos > materias > panel de materia

--- academico/views.py
+++ academico/views.py
@@ -15,15 +15,32 @@ class ListaCursosView(ListView):
         # distinct elimina duplicados
         curso_ids = asignaciones.values_list('id_curso', flat=True).distinct()
         return Curso.objects.filter(id_curso__in=curso_ids)
+
+class ListaMateriasCursoView(ListView):
+    model = ProfesorCursoAsignatura
+    template_name = 'academico/lista_materias.html'
+    context_object_name = 'materias_asignadas'
     
+    def get_queryset(self):
+        profesor_id = 1
+        id_del_curso = self.kwargs['curso_id']
+        
+        asignaciones = ProfesorCursoAsignatura.objects.filter(
+            id_profesor = profesor_id,
+            id_curso = id_del_curso
+        )
+        
+        asignatura_ids = asignaciones.values_list('id_asignatura', flat=True)
+        return Asignatura.objects.filter(id_asignatura__in=asignatura_ids)
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['curso'] = Curso.objects.get(pk=self.kwargs['curso_id'])
+        context['cursos'] = Curso.objects.get(pk=self.kwargs['curso_id'])
         return context
-    
-    
+        
+        
 class PanelMateriaView(TemplateView):
-    tamplate_name = 'academico/panel_materia.html'
+    template_name = 'academico/panel_materia.html'
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         
