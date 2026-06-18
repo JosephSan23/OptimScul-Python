0c3f7708e74a871386658ebebc8d0bcc7651d28f
Joseph <josephsantiago2345@gmail.com>
2026-06-18 14:14:05 -0500

    feat: Creando la logica para mostrar la vista de materias, estudiantes, actividades, avisos y la clase para poder crear una actividad por parte del profesor

--- academico/views.py
+++ academico/views.py
@@ -1,5 +1,11 @@
 from django.views.generic import ListView, TemplateView
-from .models import ProfesorCursoAsignatura, Curso, Asignatura
+from .models import ProfesorCursoAsignatura, Curso, Asignatura, Estudiante, Aviso, Actividad, Calificacion, EstudianteCurso
+from django.http import JsonResponse
+from django.views import View
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
+import json
+
 
 # Create your views here.
 class ListaCursosView(ListView):
@@ -46,10 +52,98 @@ class ListaMateriasCursoView(ListView):
         
 class PanelMateriaView(TemplateView):
     template_name = 'academico/panel_materia.html'
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         
-        context['curso'] = Curso.objects.get(pk=self.kwargs['curso_id'])
-        context['asignatura'] = Asignatura.objects.get(pk=self.kwargs['asignatura_id'])
+        id_del_curso = self.kwargs['curso_id']
+        id_de_la_asignatura = self.kwargs['asignatura_id']
         
+        context['curso'] = Curso.objects.get(pk=id_del_curso)
+        context['asignatura'] = Asignatura.objects.get(pk=id_de_la_asignatura)
+        
+        pca = ProfesorCursoAsignatura.objects.filter(
+            id_curso=id_del_curso,
+            id_asignatura=id_de_la_asignatura
+        ).first()
+        
+        # Pestaña 1: Avisos
+        context['publicaciones'] = Aviso.objects.filter(
+            curso_id=id_del_curso,       
+            asignatura_id=id_de_la_asignatura  
+        ).select_related('autor').order_by('-fecha_publicacion')
+
+        # Pestaña 2: Actividades
+        if pca:
+            context['asignaciones'] = Actividad.objects.filter(id_pca=pca).order_by('-fecha_limite')
+        else:
+            context['asignaciones'] = Actividad.objects.none()
+
+        # Pestaña 4: Alumnos
+        matriculas = EstudianteCurso.objects.filter(
+            id_curso=id_del_curso,
+            estado='inscrito'
+        ).select_related('id_estudiante__id_estudiante')
+    
+        
+        estudiantes = [m.id_estudiante for m in matriculas]
+        context['estudiantes'] = estudiantes
+
+        # Pestaña 3: Planilla de Calificaciones (NUEVA LÓGICA)
+        if pca:
+            calificaciones_qs = Calificacion.objects.filter(id_actividad__id_pca=pca)
+        else:
+            calificaciones_qs = Calificacion.objects.none()
+        
+        # Juntamos las notas en un diccionario rápido usando tuplas de (alumno_id, actividad_id)
+        notas_dict = {(c.id_estudiante_id, c.id_actividad_id): c.nota for c in calificaciones_qs}
+
+        # Cruzamos los alumnos con sus notas de una vez
+        planilla_alumnos = []
+        for est in estudiantes:
+            filas_actividades = []
+            for trab in context['asignaciones']:
+                nota_actual = notas_dict.get((est.id_estudiante_id, trab.id_actividad), '')
+                filas_actividades.append({
+                    'actividad_id': trab.id_actividad,
+                    'nota': nota_actual
+                })
+            planilla_alumnos.append({
+                'estudiante': est,
+                'actividades': filas_actividades
+            })
+        
+        context['planilla_alumnos'] = planilla_alumnos
+
         return context
+    
+    
+@method_decorator(csrf_exempt, name='dispatch')
+class CrearActividadView(View):
+    def post(self, request, curso_id, asignatura_id):
+        try:
+            data = json.loads(request.body)
+            
+            pca = ProfesorCursoAsignatura.objects.filter(
+                id_curso=curso_id,
+                id_asignatura=asignatura_id
+            ).first()
+            
+            if not pca:
+                return JsonResponse({'error': 'No se encontró la asignación'}, status=404)
+            
+            actividad = Actividad.objects.create(
+                nombre=data.get('nombre'),
+                descripcion=data.get('descripcion', ''),
+                fecha_limite=data.get('fecha_limite') or None,
+                id_pca=pca
+            )
+            
+            return JsonResponse({
+                'ok': True,
+                'id': actividad.id_actividad,
+                'nombre': actividad.nombre,
+                'fecha_limite': str(actividad.fecha_limite) if actividad.fecha_limite else None
+            })
+        except Exception as e:
+            return JsonResponse({'error': str(e)}, status=500)
