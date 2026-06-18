3a900f34a84ca29369de1160f92721ea88168a52
Joseph <josephsantiago2345@gmail.com>
2026-06-18 14:12:17 -0500

    feat: Añadiendo las rutas para asignaturas

--- academico/urls.py
+++ academico/urls.py
@@ -4,8 +4,11 @@ from . import views
 app_name = 'academico'
 
 urlpatterns = [
-    # Pantallas
+    # Pantallas principales
     path('mis-cursos/', views.ListaCursosView.as_view(), name='lista_cursos'),
     path('mis-cursos/<int:curso_id>/materias/', views.ListaMateriasCursoView.as_view(), name='lista_materias_curso'),
     path('mis-cursos/<int:curso_id>/materias/<int:asignatura_id>/panel/', views.PanelMateriaView.as_view(), name='panel_materia'),
+    path('mis-cursos/<int:curso_id>/materias/<int:asignatura_id>/actividades/crear/', 
+     views.CrearActividadView.as_view(), 
+     name='crear_actividad'),
 ]
