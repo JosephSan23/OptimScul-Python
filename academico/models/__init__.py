e7acd73be85b0d00e49e2a265b75b3725bd106f7
Joseph <josephsantiago2345@gmail.com>
2026-06-18 14:05:09 -0500

    feat: Agregando los modelos al init para reutilizar

--- academico/models/__init__.py
+++ academico/models/__init__.py
@@ -4,3 +4,9 @@ from .profesor import Profesor
 from .curso import Curso
 from .asignatura import Asignatura
 from .profesor_curso_asignatura import ProfesorCursoAsignatura
+
+from .estudiante import Estudiante
+from .aviso import Aviso
+from .actividad import Actividad
+from .calificacion import Calificacion
+from .estudiante_curso import EstudianteCurso
